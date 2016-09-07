5eb62286ff4ba6ab8115aa26e00757754e2aeee2
Stanley Cheung <stanleycheung@google.com>
2016-09-07 15:42:34 -0700

    Sync

--- export_to_github.sh
+++ export_to_github.sh
@@ -33,6 +33,7 @@ cp -r "$GOOGLE3"/net/grpc/gateway/runtime net/grpc/gateway
 mkdir -p net/grpc/gateway/examples
 cp -r "$GOOGLE3"/net/grpc/gateway/examples net/grpc/gateway
 rm -f net/grpc/gateway/examples/echo/BUILD
+rm -rf net/grpc/gateway/examples/streaming_server
 rm -rf javascript && mkdir -p javascript/net/grpc/web
 cp "$GOOGLE3"/javascript/net/grpc/web/*.js javascript/net/grpc/web
 cp "$GOOGLE3"/javascript/net/grpc/web/*.cc javascript/net/grpc/web
