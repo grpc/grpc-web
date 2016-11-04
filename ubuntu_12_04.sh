cdc5c3ef36fb2a2f06277decd3bb6282938019c3
Feng Li <fengli@google.com>
2016-11-04 16:04:55 -0700

    Optimize the build process to eliminate the unnecessary installation of GRPC and Protobuf.

--- ubuntu_12_04.sh
+++ ubuntu_12_04.sh
@@ -1,3 +1,5 @@
+./init_submodules.sh
+make clean
 docker build -t ubuntu_12_04 -f net/grpc/gateway/docker/ubuntu_12_04/Dockerfile .
 CONTAINER_ID=$(docker create ubuntu_12_04)
 docker cp $CONTAINER_ID:/github/grpc-web/gConnector.zip net/grpc/gateway/docker/ubuntu_12_04
--- ubuntu_14_04.sh
+++ ubuntu_14_04.sh
@@ -1,3 +1,5 @@
+./init_submodules.sh
+make clean
 docker build -t ubuntu_14_04 -f net/grpc/gateway/docker/ubuntu_14_04/Dockerfile .
 CONTAINER_ID=$(docker create ubuntu_14_04)
 docker cp $CONTAINER_ID:/github/grpc-web/gConnector.zip net/grpc/gateway/docker/ubuntu_14_04
