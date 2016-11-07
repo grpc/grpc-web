ac66a87d15970985cd393c7bfe7f31a1bb8e469f
Feng Li <fengli@google.com>
2016-11-07 10:47:18 -0800

    Correct the path in darwin_x86_64.sh to relative path, to make it portable with or without docker.

--- darwin_x86_64.sh
+++ darwin_x86_64.sh
@@ -1,6 +1,6 @@
 ./init_submodules.sh
 make clean
-cd /github/grpc-web/third_party/protobuf && ./autogen.sh && ./configure && make && cd ../..
-cd /github/grpc-web/third_party/grpc && make && cd ../..
+cd third_party/protobuf && ./autogen.sh && ./configure && make && cd ../..
+cd third_party/grpc && make && cd ../..
 export KERNEL_BITS=64
 make
