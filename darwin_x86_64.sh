4fdf3f3c190859d3dc4ceaa1f0fa27255511e1c1
Feng Li <fengli@google.com>
2016-11-17 17:45:42 -0800

    Limits the parallel build CPU to 8 on Mac OSX.

--- darwin_x86_64.sh
+++ darwin_x86_64.sh
@@ -1,6 +1,6 @@
 ./init_submodules.sh
 make clean
-cd third_party/protobuf && ./autogen.sh && ./configure && make -j && cd ../..
-cd third_party/grpc && make -j && cd ../..
+cd third_party/protobuf && ./autogen.sh && ./configure && make -j8 && cd ../..
+cd third_party/grpc && make -j8 && cd ../..
 export KERNEL_BITS=64
-make -j
+make
