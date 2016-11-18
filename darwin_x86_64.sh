b4d87e7ea9114115b602b86c2d13f0f425b3ca00
Feng Li <fengli@google.com>
2016-11-17 17:01:16 -0800

    Supports debian stretch for grpc-web. Speeds up the build process by make -j. Fix the printf portability issue on ubuntu 12.04.

--- darwin_x86_64.sh
+++ darwin_x86_64.sh
@@ -1,6 +1,6 @@
 ./init_submodules.sh
 make clean
-cd third_party/protobuf && ./autogen.sh && ./configure && make && cd ../..
-cd third_party/grpc && make && cd ../..
+cd third_party/protobuf && ./autogen.sh && ./configure && make -j && cd ../..
+cd third_party/grpc && make -j && cd ../..
 export KERNEL_BITS=64
-make
+make -j
