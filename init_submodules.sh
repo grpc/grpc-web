4fb742566a2e392e9a059a64a768b2686e7f2a11
Feng Li <fengli@google.com>
2016-11-18 18:48:31 -0800

    Remove the protobuf submodule, use the protobuf provided protobuf instead.

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,5 +1,4 @@
 git submodule update --init
 cd third_party/closure-library && git checkout tags/v20160911 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/protobuf && git checkout tags/v3.0.2 -f && cd ../..
 cd third_party/grpc && git checkout tags/v1.0.1 -f && git submodule update --init && cd ../..
