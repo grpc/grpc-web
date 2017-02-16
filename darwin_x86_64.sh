9d1a020ff8ce46854938505f2b9240fd43d70ef7
Stanley Cheung <stanleycheung@google.com>
2017-02-16 12:20:02 -0800

    Fix darwin_x86_64.sh script

--- darwin_x86_64.sh
+++ darwin_x86_64.sh
@@ -1,12 +1,19 @@
 ./init_submodules.sh
 make clean
-cd third_party/protobuf \
+cd third_party/grpc/third_party/protobuf \
   && ./autogen.sh \
   && ./configure \
-  && make install -j8 \
+  && make install \
+  && cd ../../../..
+if [[ $(uname -r) == 16* ]];
+then
+  cd third_party/grpc \
+  && CPPFLAGS=-DOSATOMIC_USE_INLINED=1 make install \
   && cd ../..
-cd third_party/grpc \
-  && make install -j8 \
+else
+  cd third_party/grpc \
+  && make install \
   && cd ../..
+fi
 export KERNEL_BITS=64
 make
