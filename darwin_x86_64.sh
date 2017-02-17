b024f276b03a6bb6be83a8ffac8fff028bfd3a1c
Stanley Cheung <stanleycheung@google.com>
2017-02-16 16:14:02 -0800

    Fix mac install script, and a bug in clientreadablestream

--- darwin_x86_64.sh
+++ darwin_x86_64.sh
@@ -3,17 +3,17 @@ make clean
 cd third_party/grpc/third_party/protobuf \
   && ./autogen.sh \
   && ./configure \
-  && make install \
+  && make install -j8 \
   && cd ../../../..
-if [[ $(uname -r) == 16* ]];
+if [[ $(uname -r) == 16* ]]; # Mac OS X Sierra
 then
   cd third_party/grpc \
-  && CPPFLAGS=-DOSATOMIC_USE_INLINED=1 make install \
-  && cd ../..
+    && CPPFLAGS=-DOSATOMIC_USE_INLINED=1 make install \
+    && cd ../..
 else
   cd third_party/grpc \
-  && make install \
-  && cd ../..
+    && make install -j8 \
+    && cd ../..
 fi
 export KERNEL_BITS=64
 make
