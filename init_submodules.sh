379adea11acfca79053f02c1a855df69a4e017c0
Feng Li <fengli@google.com>
2017-06-28 11:45:25 -0700

    Update to gRPC v1.4.1.

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,4 +1,4 @@
 git submodule update --init
 cd third_party/closure-library && git checkout tags/v20160911 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout f1666d48244143ddaf463523030ee76cc0fe691c -f && git submodule update --init && cd ../..
+cd third_party/grpc && git checkout tags/v1.4.1 -f && git submodule update --init && cd ../..
