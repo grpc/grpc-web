a160a5e6a3d4e41d8be8297ed4a428feb34db0f6
Stanley Cheung <stanleycheung@google.com>
2018-02-13 10:42:17 -0800

    Fix init_submodules script

--- scripts/init_submodules.sh
+++ scripts/init_submodules.sh
@@ -1,7 +1,8 @@
+cd "$(dirname "$0")"
 cd ..
 git submodule update --init
-cd third_party/closure-library && git checkout tags/v20160911 -f && cd ../..
+cd third_party/closure-library && git checkout tags/v20171112 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout 2b0ab320c12cb807cf05b3295b7017d0ccbf66f5 -f && git submodule update --init && cd ../..
+cd third_party/grpc && git checkout 0819ff5 -f && git submodule update --init && cd ../..
 cd scripts/
 
