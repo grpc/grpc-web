dfb2b08ce3eb289ccecc9731210c2f87b988001b
Stanley Cheung <stanleycheung@google.com>
2018-03-07 12:24:55 -0800

    gRPC Sync / Import

--- scripts/init_submodules.sh
+++ scripts/init_submodules.sh
@@ -17,4 +17,4 @@ cd "$(dirname "$0")"/..
 git submodule update --init
 cd third_party/closure-library && git checkout tags/v20171112 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout 8224c45 -f && git submodule update --init && cd ../..
+cd third_party/grpc && git checkout 8b875ac -f && git submodule update --init && cd ../..
