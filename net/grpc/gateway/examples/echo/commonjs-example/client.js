6ad3ac956cdda2108f48f4e3a9548b4c4caa7053
Stanley Cheung <stanleycheung@google.com>
2018-08-07 14:22:02 -0700

    Recursive proto demo

--- net/grpc/gateway/examples/echo/commonjs-example/client.js
+++ net/grpc/gateway/examples/echo/commonjs-example/client.js
@@ -1,4 +1,4 @@
-const {EchoRequest,
+const {Node, List, EchoRequest,
        ServerStreamingEchoRequest} = require('./echo_pb.js');
 const {EchoServiceClient} = require('./echo_grpc_pb.js');
 const {EchoApp} = require('../echoapp.js');
@@ -6,6 +6,17 @@ const grpc = {};
 grpc.web = require('grpc-web');
 
 var echoService = new EchoServiceClient('http://localhost:8080', null, null);
+
+var node1 = new Node();
+node1.setVal("node1");
+
+var list = new List();
+list.setNodesList([node1]);
+
+var node2 = new Node();
+node2.setList(list);
+
+console.log("node1.val = "+node2.getList().getNodesList()[0].getVal());
 
 var echoApp = new EchoApp(
   echoService,
