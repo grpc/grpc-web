359c5e3023c3f1b59f7c61db4f9322b5f79d467e
Stanley Cheung <stanleycheung@google.com>
2018-10-02 18:01:04 -0700

    Add a simple Hello World Guide

---
+++ net/grpc/gateway/examples/echo/helloworld/server.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var PROTO_PATH = __dirname + '/helloworld.proto';
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+var helloworld = protoDescriptor.helloworld;
+
+function doSayHello(call, callback) {
+  callback(null, {
+    message: 'Hello! ' + call.request.name
+  });
+}
+
+function getServer() {
+  var server = new grpc.Server();
+  server.addProtoService(helloworld.Greeter.service, {
+    sayHello: doSayHello,
+  });
+  return server;
+}
+
+if (require.main === module) {
+  var server = getServer();
+  server.bind('0.0.0.0:9090', grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+exports.getServer = getServer;
