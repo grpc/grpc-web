f92cedafbf833d3df1452509483da1ad8d07124d
Stanley Cheung <stanleycheung@google.com>
2018-10-06 00:49:29 -0700

    Show how deadline can be set

--- net/grpc/gateway/examples/helloworld/client.js
+++ net/grpc/gateway/examples/helloworld/client.js
@@ -16,15 +16,39 @@
  *
  */
 
-const {HelloRequest, HelloReply} = require('./helloworld_pb.js');
+const {HelloRequest, RepeatHelloRequest,
+       HelloReply} = require('./helloworld_pb.js');
 const {GreeterClient} = require('./helloworld_grpc_web_pb.js');
 
 var client = new GreeterClient('http://' + window.location.hostname + ':8080',
                                null, null);
 
+// simple unary call
 var request = new HelloRequest();
 request.setName('World');
 
 client.sayHello(request, {}, (err, response) => {
   console.log(response.getMessage());
 });
+
+
+// server streaming call
+var streamRequest = new RepeatHelloRequest();
+streamRequest.setName('World');
+streamRequest.setCount(5);
+
+var stream = client.sayRepeatHello(streamRequest, {});
+stream.on('data', (response) => {
+  console.log(response.getMessage());
+});
+  
+
+// deadline exceeded
+var deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() + 1);
+
+client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
+  (err, response) => {
+    console.log('Got error, code = ' + err.code +
+                ', message = ' + err.message);
+  });
--- net/grpc/gateway/examples/helloworld/server.js
+++ net/grpc/gateway/examples/helloworld/server.js
@@ -19,6 +19,8 @@
 var PROTO_PATH = __dirname + '/helloworld.proto';
 
 var grpc = require('grpc');
+var _ = require('lodash');
+var async = require('async');
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -32,8 +34,37 @@ var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 var helloworld = protoDescriptor.helloworld;
 
 function doSayHello(call, callback) {
-  callback(null, {
-    message: 'Hello! ' + call.request.name
+  callback(null, {message: 'Hello! '+ call.request.name});
+}
+
+function doSayRepeatHello(call) {
+  var senders = [];
+  function sender(name) {
+    return (callback) => {
+      call.write({
+        message: 'Hey! ' + name
+      });
+      _.delay(callback, 500); // in ms
+    };
+  }
+  for (var i = 0; i < call.request.count; i++) {
+    senders[i] = sender(call.request.name + i);
+  }
+  async.series(senders, () => {
+    call.end();
+  });
+}
+
+function doSayHelloAfterDelay(call, callback) {
+  function dummy() {
+    return (cb) => {
+      _.delay(cb, 5000);
+    };
+  }
+  async.series([dummy()], () => {
+    callback(null, {
+      message: 'Hello! '+call.request.name
+    });
   });
 }
 
@@ -41,6 +72,8 @@ function getServer() {
   var server = new grpc.Server();
   server.addProtoService(helloworld.Greeter.service, {
     sayHello: doSayHello,
+    sayRepeatHello: doSayRepeatHello,
+    sayHelloAfterDelay: doSayHelloAfterDelay
   });
   return server;
 }
