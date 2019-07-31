d9af86398b15a66d94c28b75f66586aa863387ab
Stanley Cheung <stanleycheung@google.com>
2019-07-31 15:58:56 -0700

    Adding some groundwork for generic client

---
+++ javascript/net/grpc/web/metadata.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview grpc-web request/response metadata.
+ *
+ * Request and response headers will be included in the Metadata.
+ */
+
+goog.module('grpc.web.Metadata');
+goog.module.declareLegacyNamespace();
+
+/**
+ * @typedef {!Object<string,string>}
+ */
+let Metadata;
+
+exports = Metadata;
