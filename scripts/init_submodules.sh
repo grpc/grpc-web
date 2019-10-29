3ff3d6f15325cab6f96b33b0d4f8f0c03be063dc
Stanley Cheung <stanleycheung@google.com>
2019-10-29 13:21:37 -0700

    Update third_party hashes and package.json

--- scripts/init_submodules.sh
+++ scripts/init_submodules.sh
@@ -18,4 +18,4 @@ cd "$(dirname "$0")"/..
 git submodule update --init
 cd third_party/closure-library && git checkout tags/v20171112 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout d8772cf -f && git submodule update --init && cd ../..
+cd third_party/grpc && git checkout 2de2e8d -f && git submodule update --init && cd ../..
