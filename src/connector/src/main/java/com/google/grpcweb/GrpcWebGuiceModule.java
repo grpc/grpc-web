6e9dc4a4a6965c76c7508fc20600747f24ee5ee2
Vasu Nori <vnori@google.com>
2020-06-09 18:04:48 -0700

    Add Dependency Injection(Guice) and copyright notice

--- src/connector/src/main/java/com/google/grpcweb/GrpcWebGuiceModule.java
+++ src/connector/src/main/java/com/google/grpcweb/GrpcWebGuiceModule.java
@@ -15,29 +15,18 @@
  */
 package com.google.grpcweb;
 
-/**
- * Creates frames from the input bytes.
- */
-class MessageFramer {
-  enum Type {
-    DATA ((byte) 0x00),
-    TRAILER ((byte) 0x80);
+import com.google.inject.AbstractModule;
 
-    public final byte value;
-    Type(byte b) {
-      value = b;
-    }
+public class GrpcWebGuiceModule extends AbstractModule {
+  private static int sGrpcPortNum = 0;
+
+  public static void setGrpcPortNum(int i) {
+    if (sGrpcPortNum == 0) sGrpcPortNum = i;
   }
 
-  // TODO: handle more than single frame; i.e., input byte array size > (2GB - 1)
-  byte[] getPrefix(byte[] in, Type type) {
-    int len = in.length;
-    return new byte[] {
-        type.value,
-        (byte) ((len >> 24) & 0xff),
-        (byte) ((len >> 16) & 0xff),
-        (byte) ((len >> 8) & 0xff),
-        (byte) ((len >> 0) & 0xff),
-    };
+  @Override
+  protected void configure() {
+    bind(GrpcServiceConnectionManager.class)
+        .toInstance(new GrpcServiceConnectionManager(sGrpcPortNum));
   }
 }
--- src/connector/src/main/java/com/google/grpcweb/GrpcWebTrafficServlet.java
+++ src/connector/src/main/java/com/google/grpcweb/GrpcWebTrafficServlet.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2020  Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.grpcweb;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +29,13 @@ public class GrpcWebTrafficServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    new RequestHandler(mFactory).handle(request, response);
+    Injector injector = Guice.createInjector(new GrpcWebGuiceModule());
+    RequestHandler reqHandler = injector.getInstance(RequestHandler.class);
+    reqHandler.handle(request, response);
   }
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     doGet(request, response);
-  }
-
-  private final Factory mFactory;
-
-  public GrpcWebTrafficServlet() {
-    mFactory = Factory.getInstance();
   }
 }
