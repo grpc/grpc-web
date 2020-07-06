c0ecfa5220ff370ecfa1c6b9dbeb2106d0e5390d
Stanley Cheung <stanleycheung@google.com>
2020-07-06 14:33:24 -0700

    Add @interface MethodDescroptorInterface

--- javascript/net/grpc/web/methoddescriptor.js
+++ javascript/net/grpc/web/methoddescriptor.js
@@ -9,6 +9,7 @@ goog.module.declareLegacyNamespace();
 
 const CallOptions = goog.require('grpc.web.CallOptions');
 const Metadata = goog.requireType('grpc.web.Metadata');
+const MethodDescriptorInterface = goog.requireType('grpc.web.MethodDescriptorInterface');
 const MethodType = goog.requireType('grpc.web.MethodType');
 const Request = goog.requireType('grpc.web.Request');
 const RequestInternal = goog.require('grpc.web.RequestInternal');
@@ -16,50 +17,51 @@ const UnaryResponse = goog.requireType('grpc.web.UnaryResponse');
 const UnaryResponseInternal = goog.require('grpc.web.UnaryResponseInternal');
 const {Status} = goog.requireType('grpc.web.Status');
 
-/** @template REQUEST, RESPONSE */
-class MethodDescriptor {
-  /**
-   * @param {string} name
-   * @param {!MethodType} methodType
-   * @param {function(new: REQUEST, ...)} requestType
-   * @param {function(new: RESPONSE, ...)} responseType
-   * @param {function(REQUEST): ?} requestSerializeFn
-   * @param {function(?): RESPONSE} responseDeserializeFn
-   */
-  constructor(
-      name, methodType, requestType, responseType, requestSerializeFn,
-      responseDeserializeFn) {
-    /** @const */
-    this.name = name;
-    /** @const */
-    this.methodType = methodType;
-    /** @const */
-    this.requestType = requestType;
-    /** @const */
-    this.responseType = responseType;
-    /** @const */
-    this.requestSerializeFn = requestSerializeFn;
-    /** @const */
-    this.responseDeserializeFn = responseDeserializeFn;
-  }
+/**
+ * @constructor
+ * @final
+ * @implements {MethodDescriptorInterface}
+ * @template REQUEST, RESPONSE
+ * @param {string} name
+ * @param {?MethodType} methodType
+ * @param {function(new: REQUEST, ?Array=)} requestType
+ * @param {function(new: RESPONSE, ?Array=)} responseType
+ * @param {function(REQUEST): ?} requestSerializeFn
+ * @param {function(?): RESPONSE} responseDeserializeFn
+ */
+const MethodDescriptor = function(
+    name, methodType, requestType, responseType, requestSerializeFn,
+    responseDeserializeFn) {
+  /** @const */
+  this.name = name;
+  /** @const */
+  this.methodType = methodType;
+  /** @const */
+  this.requestType = requestType;
+  /** @const */
+  this.responseType = responseType;
+  /** @const */
+  this.requestSerializeFn = requestSerializeFn;
+  /** @const */
+  this.responseDeserializeFn = responseDeserializeFn;
+};
 
-  /**
-   * @template REQUEST, RESPONSE
-   * @param {REQUEST} requestMessage
-   * @param {!Metadata=} metadata
-   * @param {!CallOptions=} callOptions
-   * @return {!Request<REQUEST, RESPONSE>}
-   */
-  createRequest(
-      requestMessage, metadata = {}, callOptions = new CallOptions()) {
-    return new RequestInternal(requestMessage, this, metadata, callOptions);
-  }
-}
+/**
+ * @override
+ * @param {REQUEST} requestMessage
+ * @param {!Metadata=} metadata
+ * @param {!CallOptions=} callOptions
+ * @return {!Request<REQUEST, RESPONSE>}
+ */
+MethodDescriptor.prototype.createRequest = function(
+    requestMessage, metadata = {}, callOptions = new CallOptions()) {
+  return new RequestInternal(requestMessage, this, metadata, callOptions);
+};
 
 
 
 /**
- * @template REQUEST, RESPONSE
+ * @override
  * @param {RESPONSE} responseMessage
  * @param {!Metadata=} metadata
  * @param {?Status=} status
