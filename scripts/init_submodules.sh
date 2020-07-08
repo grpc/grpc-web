d4e0543c3c6b1a8b810fbcccda7960fdeee2a476
Stanley Cheung <stanleycheung@google.com>
2020-07-07 17:07:23 -0700

    Update init_submodules.sh

--- scripts/init_submodules.sh
+++ scripts/init_submodules.sh
@@ -18,5 +18,5 @@ cd "$(dirname "$0")"/..
 git submodule --quiet update --init
 cd third_party/closure-library && git checkout tags/v20200406 -f && cd ../..
 cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout tags/v1.28.1 -f && \
+cd third_party/grpc && git checkout tags/v1.30.0 -f && \
   git submodule --quiet update --init && cd ../..
