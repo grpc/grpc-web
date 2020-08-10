a84902c18f5f9b18d2450c8d3cea21d06fc40590
Stanley Cheung <stanleycheung@google.com>
2020-08-09 23:06:44 -0700

    Migrate to ES6 classes

--- javascript/net/grpc/web/methoddescriptor.js
+++ javascript/net/grpc/web/methoddescriptor.js
@@ -18,102 +18,101 @@ const UnaryResponseInternal = goog.require('grpc.web.UnaryResponseInternal');
 const {Status} = goog.requireType('grpc.web.Status');
 
 /**
- * @constructor
  * @final
  * @implements {MethodDescriptorInterface<REQUEST, RESPONSE>}
  * @template REQUEST, RESPONSE
- * @param {string} name
- * @param {?MethodType} methodType
- * @param {function(new: REQUEST, ...)} requestType
- * @param {function(new: RESPONSE, ...)} responseType
- * @param {function(REQUEST): ?} requestSerializeFn
- * @param {function(?): RESPONSE} responseDeserializeFn
+ * @unrestricted
  */
-const MethodDescriptor = function(
-    name, methodType, requestType, responseType, requestSerializeFn,
-    responseDeserializeFn) {
-  /** @const */
-  this.name = name;
-  /** @const */
-  this.methodType = methodType;
-  /** @const */
-  this.requestType = requestType;
-  /** @const */
-  this.responseType = responseType;
-  /** @const */
-  this.requestSerializeFn = requestSerializeFn;
-  /** @const */
-  this.responseDeserializeFn = responseDeserializeFn;
+const MethodDescriptor = class {
+  /**
+   * @param {string} name
+   * @param {?MethodType} methodType
+   * @param {function(new: REQUEST, ...)} requestType
+   * @param {function(new: RESPONSE, ...)} responseType
+   * @param {function(REQUEST): ?} requestSerializeFn
+   * @param {function(?): RESPONSE} responseDeserializeFn
+   */
+  constructor(
+      name, methodType, requestType, responseType, requestSerializeFn,
+      responseDeserializeFn) {
+    /** @const */
+    this.name = name;
+    /** @const */
+    this.methodType = methodType;
+    /** @const */
+    this.requestType = requestType;
+    /** @const */
+    this.responseType = responseType;
+    /** @const */
+    this.requestSerializeFn = requestSerializeFn;
+    /** @const */
+    this.responseDeserializeFn = responseDeserializeFn;
+  }
+
+  /**
+   * @override
+   * @param {REQUEST} requestMessage
+   * @param {!Metadata=} metadata
+   * @param {!CallOptions=} callOptions
+   * @return {!Request<REQUEST, RESPONSE>}
+   */
+  createRequest(
+      requestMessage, metadata = {}, callOptions = new CallOptions()) {
+    return new RequestInternal(requestMessage, this, metadata, callOptions);
+  }
+
+  /**
+   * @override
+   * @param {RESPONSE} responseMessage
+   * @param {!Metadata=} metadata
+   * @param {?Status=} status
+   * @return {!UnaryResponse<REQUEST, RESPONSE>}
+   */
+  createUnaryResponse(responseMessage, metadata = {}, status = null) {
+    return new UnaryResponseInternal(responseMessage, this, metadata, status);
+  }
+
+  /**
+   * @override
+   */
+  getName() {
+    return this.name;
+  }
+
+  /**
+   * @override
+   */
+  getMethodType() {
+    return this.methodType;
+  }
+
+  /**
+   * @override
+   * @return {function(new: RESPONSE, ...)}
+   */
+  getResponseMessageCtor() {
+    return this.responseType;
+  }
+
+  /**
+   * @override
+   * @return {function(new: REQUEST, ...)}
+   */
+  getRequestMessageCtor() {
+    return this.requestType;
+  }
+
+  /** @override */
+  getResponseDeserializeFn() {
+    return this.responseDeserializeFn;
+  }
+
+  /** @override */
+  getRequestSerializeFn() {
+    return this.requestSerializeFn;
+  }
 };
 
-/**
- * @override
- * @param {REQUEST} requestMessage
- * @param {!Metadata=} metadata
- * @param {!CallOptions=} callOptions
- * @return {!Request<REQUEST, RESPONSE>}
- */
-MethodDescriptor.prototype.createRequest = function(
-    requestMessage, metadata = {}, callOptions = new CallOptions()) {
-  return new RequestInternal(requestMessage, this, metadata, callOptions);
-};
 
-
-
-/**
- * @override
- * @param {RESPONSE} responseMessage
- * @param {!Metadata=} metadata
- * @param {?Status=} status
- * @return {!UnaryResponse<REQUEST, RESPONSE>}
- */
-MethodDescriptor.prototype.createUnaryResponse = function(
-    responseMessage, metadata = {}, status = null) {
-  return new UnaryResponseInternal(responseMessage, this, metadata, status);
-};
-
-
-/**
- * @override
- */
-MethodDescriptor.prototype.getName = function() {
-  return this.name;
-};
-
-/**
- * @override
- */
-MethodDescriptor.prototype.getMethodType = function() {
-  return this.methodType;
-};
-
-/**
- * @override
- * @return {function(new: RESPONSE, ...)}
- */
-MethodDescriptor.prototype.getResponseMessageCtor = function() {
-  return this.responseType;
-};
-
-
-/**
- * @override
- * @return {function(new: REQUEST, ...)}
- */
-MethodDescriptor.prototype.getRequestMessageCtor = function() {
-  return this.requestType;
-};
-
-
-/** @override */
-MethodDescriptor.prototype.getResponseDeserializeFn = function() {
-  return this.responseDeserializeFn;
-};
-
-
-/** @override */
-MethodDescriptor.prototype.getRequestSerializeFn = function() {
-  return this.requestSerializeFn;
-};
 
 exports = MethodDescriptor;
