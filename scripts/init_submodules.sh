727ba1b6b15f310f7ee45b346951f892b988c0f8
Stanley Cheung <stanleycheung@google.com>
2021-03-14 01:42:29 -0800

    Removed old nginx gateway code

--- scripts/init_submodules.sh
+++ scripts/init_submodules.sh
@@ -15,8 +15,6 @@
 set -ex
 
 cd "$(dirname "$0")"/..
-git submodule --quiet update --init
-cd third_party/closure-library && git checkout tags/v20201102 -f && cd ../..
-cd third_party/openssl && git checkout tags/OpenSSL_1_0_2h -f && cd ../..
-cd third_party/grpc && git checkout tags/v1.30.0 -f && \
-  git submodule --quiet update --init && cd ../..
+git submodule --quiet update --init --recursive
+(cd third_party/closure-library && git checkout tags/v20201102)
+(cd third_party/protobuf && git checkout tags/v3.15.6)
