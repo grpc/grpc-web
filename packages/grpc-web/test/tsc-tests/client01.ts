9c7868ab9ff5f022a2e081259c4d32c77d6a52d1
Noel Kim (김민혁) <noel@chequer.io>
2022-04-26 15:46:48 +0900

    Fix for JS Identifier

--- packages/grpc-web/test/tsc-tests/client01.ts
+++ packages/grpc-web/test/tsc-tests/client01.ts
@@ -22,6 +22,6 @@ import {MessageOuter} from './generated/test01_pb';
 let inner1 = new MessageOuter.MessageInner();
 inner1.setValue(123);
 let msgOuter = new MessageOuter();
-msgOuter.setSomepropList([inner1]);
+msgOuter.setSomePropList([inner1]);
 
 export {msgOuter}
